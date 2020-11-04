13328743a5dee75446b526ca341b5b5fe6bfc984
Nenad Sparavalo <nenadsparavalo@Nenads-MacBook-Pro.local>
2020-11-04 16:34:13 +0100

    Something

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { corona } from "../src/components/data/data";
+import Speech from "speak-tts";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const speech = new Speech();
+	speech.init({
+		volume: 1,
+		lang: "de",
+		rate: 0.7,
+		pitch: 1,
+		splitSentences: true,
+	});
+
+	let [addOne, setAddOne] = useState(0);
+	const [german, setGerman] = useState(corona[addOne].german);
+	const [english, setEnglish] = useState(corona[addOne].english);
+	const [disabled, setDisabled] = useState(false);
+
+	const startCount = () => {
+		setAddOne(addOne + 1);
+	};
+
+	useEffect(() => {
+		startCount();
+	}, []);
+
+	const listenGerman = () => {
+		speech.speak({ text: german });
+	};
+	const nextWord = () => {
+		setAddOne(addOne + 1);
+		setGerman(corona[addOne].german);
+		setEnglish(corona[addOne].english);
+		if (addOne === corona.length - 2) {
+			setDisabled({ disabled: true });
+		}
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="container">
+					<div className="row">
+						<div className="col-12">
+							<div className="card bg-dark">
+								<h2 className="card-header py-4" onClick={() => listenGerman()}>
+									{german}
+								</h2>
+								<h3 className="card-body text-capitalize">{english}</h3>
+							</div>
+						</div>
+						<div className="col-12">
+							<button
+								disabled={disabled}
+								onClick={() => nextWord(german)}
+								type="button"
+								className="btn btn-lg btn-danger fixed-bottom btn-block"
+							>
+								Next word
+							</button>
+						</div>
+					</div>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/data/data.js
@@ -0,0 +1,122 @@
+export const corona = [
+	{
+		german: "die Maske",
+		english: "the mask",
+		id: "1",
+	},
+	{
+		german: "das Fieber",
+		english: "the fever",
+		id: "2",
+	},
+	{
+		german: "der Schnupfen",
+		english: "the cold",
+		id: "3",
+	},
+	{
+		german: "die Quarantäne",
+		english: "the quarantine",
+		id: "4",
+	},
+	{
+		german: "der Sicherheitsabstand",
+		english: "the social distance",
+		id: "5",
+	},
+	{
+		german: "Gesundheit!",
+		english: "Bless you!",
+		id: "6",
+	},
+	{
+		german: "Bitte desinfizieren Sie sich die Hände.",
+		english: "Please desinfect your hands.",
+		id: "7",
+	},
+	{
+		german: "Haben Sie eine Frage ?",
+		english: "Do you have a question ?",
+		id: "8",
+	},
+	{
+		german: "Kann ich Ihnen helfen ?",
+		english: "May i help you ?",
+		id: "9",
+	},
+	{
+		german: "Dieser Bereich ist gespertt.",
+		english: "This area is closed.",
+		id: "10",
+	},
+	{
+		german: "Bitte tragen Sie ihre Maske.",
+		english: "Please wear your mask.",
+		id: "11",
+	},
+	{
+		german: "Bitte tragen Sie Ihre Maske über Nase und Mund.",
+		english: "Please wear your mask covering nose and mouth.",
+		id: "12",
+	},
+	{
+		german: "Es gibt eine Maskenpflicht an Board.",
+		english: "Face masks are required on board.",
+		id: "13",
+	},
+	{
+		german: "Bitte halten Sie 1,5 Meter Abstand.",
+		english: "Please keep a distance of 1.5 meters",
+		id: "14",
+	},
+	{
+		german: "Bitte beachten Sie den Sicherheitsabstand.",
+		english: "Please mind social distance",
+		id: "15",
+	},
+	{
+		german: "Wie geht es Ihnen heute ?",
+		english: "How do you feel today ?",
+		id: "16",
+	},
+	{
+		german: "Alles gut ?",
+		english: "Everything ok ?",
+		id: "17",
+	},
+	{
+		german: "Fühlen Sie sich gut ?",
+		english: "Are you feeling Well ?",
+		id: "18",
+	},
+	{
+		german: "Haben Sie Beschwerden ?",
+		english: "Are you having any complaints ?",
+		id: "19",
+	},
+	{
+		german: "Bitte rufen Sie die Rezeption an.",
+		english: "Please call the reception.",
+		id: "20",
+	},
+	{
+		german: "Bitte gehen Sie zum Hospital auf Deck 3.",
+		english: "Please go to our hospital at Deck 3.",
+		id: "21",
+	},
+	{
+		german: "Der Corona-Test wird im Hospital durchgeführt.",
+		english: "The corona-test will be conducted in the hospital.",
+		id: "22",
+	},
+	{
+		german: "Bitte gehen Sie auf ihre Kabine.",
+		english: "Please kindly go to your cabin.",
+		id: "23",
+	},
+	{
+		german: "",
+		english: "",
+		id: "",
+	},
+];
